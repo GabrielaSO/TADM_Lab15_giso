5f5e8561e879ca7bb45d51da9ad04c9a015f85f3
GabrielaSO <gaitsotoor@ittepic.edu.mx>
2018-02-12 08:52:27 -0700

    Initial commit

---
+++ app/src/main/java/com/example/giso/tadm_lab15_giso/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.giso.tadm_lab15_giso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
